1acdb2db1cbdcbf37d7e0b16df9665dd8eb47c1f
Zarha Buske <zarha.buske@medieinstitutet.se>
2022-03-07 18:57:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './styles/App.css';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from '../images/profile.jpg'
+import '../styles/Header.css'
+
+
+export default function Header() {
+  return (
+    <header>
+      <img src={Image} alt="" />
+      
+      <div className="header-info">
+        <h1>Zarha Buske</h1>
+        <h4>Web developer</h4>
+        <h6>website comming soon</h6>
+      </div>
+    </header>
+  )
+}
