bc3f7c58f9a8d6db31fde1d87a91b3eca172535b
Zarha Buske <zarha.buske@medieinstitutet.se>
2022-03-08 10:54:19 +0100

    final

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import '../styles/Info.css'
+import React from "react";
+import "../styles/Info.css";
 
 export default function Info() {
-  return (
-    <div className="info-container">
-        <div className="about">
-            <h2>About</h2>
-            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
-        </div>
-        <div className="interests">
-            <h2>Interests</h2>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>    
-        </div>
-    </div>
-  )
+	return (
+		<div className="info-container">
+			<div className="about">
+				<h2>About</h2>
+				<p>
+					I am curious and motivated for a new career in web development. I look
+					forward to new challenges and see great value in learning new tools
+					and being able to deepen and develop my own knowledge and skills.
+				</p>
+			</div>
+			<div className="interests">
+				<h2>Interests</h2>
+				<p>
+					DIY expert. Reader. Internet fanatic. Bacon buff.
+					Hobby photographer. Travel geek. Pop culture ninja. Fika fanatic.
+				</p>
+			</div>
+		</div>
+	);
 }
